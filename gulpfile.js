4dbf6cc1026b1c05f63004e2cd3f5bf4b783a770
Aaron Zhang <aaron@thinkero.us>
2014-11-10 23:57:28 -0500

    Remove react lib from dist

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ gulp.task('default', function() {
   var bundler = browserify('./src/index.jsx', { debug: true, extensions: [".jsx"] });
   bundler.transform(reactify, {es6: true});
   bundler.transform(es6ify.configure(/^(?!.*node_modules)+.+\.js(x?)$/));
+  bundler.external('react');
 
   return bundler.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
